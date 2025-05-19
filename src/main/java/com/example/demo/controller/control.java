21d3d0b04cb5b016bbf7190a945debc505ab467b
Harshit Aggarwal <103198026+2003Harshit@users.noreply.github.com>
2025-05-19 15:26:00 +0530

    added spring project

---
+++ src/main/java/com/example/demo/controller/control.java
@@ -0,0 +1,21 @@
+
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class control {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from BridgeLabz";
+    }
+    @GetMapping("/user")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+	    }
+
+    
+}
